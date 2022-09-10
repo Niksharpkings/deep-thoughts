cbff7b71b551aa526a17830f71b6ecadf540ee07
Nik Sharpio <niksharpkings@gmail.com>
2022-09-09 21:07:33 -0700

    21.4

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy; 2022 Start-Code by Lernantino, Edited By Nikita "Nik" Sharpio for UCLAx-U2 The Coding Boot Camp</div>
+    </footer>
+  );
+};
+
+export default Footer;
