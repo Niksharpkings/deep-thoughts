d8632d4e90832b9d424454d629ba9599328cf8e5
Nik Sharpio <niksharpkings@gmail.com>
2022-09-10 18:04:34 -0700

    21.6

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ThoughtList from "../components/ThoughtList";
-
+import ThoughtForm from '../components/ThoughtForm';
 import { useQuery } from "@apollo/client";
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
 import Auth from "../utils/auth";
 import FriendList from "../components/FriendList";
+
 
 const Home = () => {
   // use useQuery Hook to make query request and store response data in a variable called userData (we'll use this to conditionally render the FriendList component) and thoughtData (we'll use this to conditionally render the ThoughtList component)
@@ -20,6 +21,11 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
+      {loggedIn && (
+      <div className="col-12 mb-3">
+        <ThoughtForm />
+      </div>
+    )}
         <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
           {loading ? (
             <div>Loading...</div>
